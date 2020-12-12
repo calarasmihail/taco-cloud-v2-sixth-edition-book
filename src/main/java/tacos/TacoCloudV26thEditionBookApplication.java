81212ddf3513dbb3c8b13536d20d914af20af9c7
Mihail Calaras <mihail.calaras@reigncode.dev>
2020-12-13 00:19:09 +0200

    second commit

---
+++ src/main/java/tacos/TacoCloudV26thEditionBookApplication.java
@@ -0,0 +1,13 @@
+package tacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudV26thEditionBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoCloudV26thEditionBookApplication.class, args);
+	}	
+
+}
